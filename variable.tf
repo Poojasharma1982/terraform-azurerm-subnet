69965d75722b3fd7bd033c61d0b3db5c5d9bb437
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-30 16:46:23 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "subnet1-name" {
+  type = list
+  #default = "01000000-0000-4000-8000-000030080200"
+  
+}
+variable "vnet-name" {
+  type = list
+  #default = "01000000-0000-4000-8000-000030080200"
+  
+}
+variable "rg-name" {
+  type = string
+  #default = "01000000-0000-4000-8000-000030080200"
+  
+}
