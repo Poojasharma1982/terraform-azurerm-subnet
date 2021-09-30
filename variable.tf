e92deceffbb49cece167f57cb636bdaa286a8ff3
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-30 17:17:45 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,10 @@
 variable "subnet1-name" {
-  type = list
+  type = string
   #default = "01000000-0000-4000-8000-000030080200"
   
 }
 variable "vnet-name" {
-  type = list
+  type = string
   #default = "01000000-0000-4000-8000-000030080200"
   
 }
