fe433ebaf71c0e181fefdbe3deb08a5df5958071
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-30 17:11:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ resource "azurerm_subnet" "mysubnet1" {
   virtual_network_name = var.vnet-name
   # resource_group_name  = azurerm_resource_group.mysubnet1.name
   # virtual_network_name = azurerm_virtual_network.mysubnet1.name
-  address_prefixes     = ["10.0.1.0/24"]
+  # address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes = var.subnet1-address
 
   delegation {
     name = "delegation"
