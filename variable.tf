ed92c9e50e856c0e0a83bb29e8b46092af5169e4
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-30 21:26:39 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,10 +2,6 @@ variable "vnet-name" {
   type = string
   #default = "01000000-0000-4000-8000-000030080200"
 }
-variable "vnet_address" {
-  type = list
-  #default = "01000000-0000-4000-8000-000030080200"
-}
 variable "rg_name_new" {
   type = string
   #default = "01000000-0000-4000-8000-000030080200"
